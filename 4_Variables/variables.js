fb576003918c5588a4d2f05e23bbf077f000a9f9
Nandhini Kalliyappan <nandhinikalliyappan@gmail.com>
2026-06-23 09:57:47 +0530

    XCommiting variables.js file

---
+++ 4_Variables/variables.js
@@ -0,0 +1,19 @@
+//variable Declaration
+let bookName;
+
+//variable Initialization
+bookName = "The Great Gatsby";
+
+//variable Declaration and Initialization
+let bookAuthor = "F. Scott Fitzgerald";
+
+console.log("Book Name: " + bookName);
+console.log("Book Author: " + bookAuthor);
+
+//reassignment of new value to the variable
+bookName = "To Kill a Mockingbird";
+
+
+
+console.log("Updated Book Name: " + bookName);
+

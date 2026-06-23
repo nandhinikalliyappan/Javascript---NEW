30d6d9c76bf32a9272d142e6ee774f1cee6139b2
Nandhini Kalliyappan <nandhinikalliyappan@gmail.com>
2026-06-23 14:03:44 +0530

    commiting constant.js

---
+++ 5_Constants/constants.js
@@ -0,0 +1,14 @@
+const BOOKNAME = "The Great Gatsby";
+
+console.log("Book Name: " + BOOKNAME);
+
+// This will throw an error because BOOKNAME2 is a constant and cannot be reassigned.
+
+const BOOKNAME= "To Kill a Mockingbird";  
+
+console.log("Updated Book Name: " + BOOKNAME);
+
+
+
+
+
